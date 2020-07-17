22391ef016f89a7406c4fd09ea1d9a11a0924d64
Martijn ten Caat <martijn.tencaat@edsn.nl>
2020-07-17 12:02:37 +0200

    Moved code to main class

--- src/main/java/crm/application/CRMApplication.java
+++ src/main/java/crm/application/CRMApplication.java
@@ -1,11 +1,14 @@
 package crm.application;
 
+import crm.database.DBconnect;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class CRMApplication {
-    public static void main(String[] args) {
-        System.out.printf("Hello world!");
-
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("add contactname:");
         String name = new Scanner(System.in).nextLine();
 
@@ -14,6 +17,19 @@ public class CRMApplication {
 
         Contacts contact = new Contacts(surname, name);
         System.out.println("Added one contact: " + contact.getContact());
+
+        DBconnect con = new DBconnect();
+
+        Statement sqlStatement = con.getConnection().createStatement();
+        String tableName = "contact";
+        ResultSet resultSet = sqlStatement.executeQuery("SELECT * FROM " + tableName + ";");
+
+        while (resultSet.next()) {
+            System.out.print(resultSet.getInt(1));
+            System.out.print(resultSet.getString(2));
+            System.out.print(resultSet.getString(3));
+            System.out.println(resultSet.getString(4));
+        }
 
     }
 }
